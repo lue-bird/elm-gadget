251241e20789c593575db7683579a5ff060747cf
Ed Kelly <edwardjkelly@gmail.com>
2026-05-11 08:30:17 +0000

    Add (failing) tests for variant3, 4 and 5

--- tests/TestHelpers.elm
+++ tests/TestHelpers.elm
@@ -28,12 +28,15 @@ type Custom
     = Var0
     | Var1 (List Bool)
     | Var2 Int ( Bool, Char )
+    | Var3 Bool Bool Bool
+    | Var4 Bool Bool Bool Bool
+    | Var5 Bool Bool Bool Bool Bool
 
 
 customCodec : IR.Codec Custom Custom
 customCodec =
     IR.custom
-        (\v0 v1 v2 v ->
+        (\v0 v1 v2 v3 v4 v5 v ->
             case v of
                 Var0 ->
                     v0
@@ -43,8 +46,20 @@ customCodec =
 
                 Var2 i r ->
                     v2 i r
+
+                Var3 b1 b2 b3 ->
+                    v3 b1 b2 b3
+
+                Var4 b1 b2 b3 b4 ->
+                    v4 b1 b2 b3 b4
+
+                Var5 b1 b2 b3 b4 b5 ->
+                    v5 b1 b2 b3 b4 b5
         )
         |> IR.variant0 Var0
         |> IR.variant1 Var1 (IR.list IR.bool)
         |> IR.variant2 Var2 IR.int (IR.tuple IR.bool IR.char)
+        |> IR.variant3 Var3 IR.bool IR.bool IR.bool
+        |> IR.variant4 Var4 IR.bool IR.bool IR.bool IR.bool
+        |> IR.variant5 Var5 IR.bool IR.bool IR.bool IR.bool IR.bool
         |> IR.endCustom
